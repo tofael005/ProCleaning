32acbf42b6724134d384f5ac3e93f75d9da10b9f
Arnob <arnobtuhin.j@gamil.com>
2025-05-17 23:38:39 +0600

    foot

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='container mx-auto px-2'>
-            <h1>Footer.</h1>
+            <h1>Footer</h1>
         </div>
     );
 };
